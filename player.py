69f8b7e64435a9cc8202d96c3014ba6044a47cce
Howon Lee <howon@thebackplane.com>
2013-10-18 21:06:11 -0700

    add proper note moving

--- player.py
+++ player.py
@@ -17,31 +17,35 @@ def getNoteColor(note):
     g = (notehash & 0x00FF00) >> 8
     b = (notehash & 0x0000FF)
     return pygame.Color(r, g, b)
+def makeNoteRect(note, height):
+    left = ((winHeight / len(currNoteState)) * note) + 5
+    top = (winWidth / 4) * 3
+    width = 50
+    return [note, [left, top, width, height]]
 
 mousex, mousey = 0, 0
 currNoteState = [0, 0, 0]
 keyRects = []
 for note, val in enumerate(currNoteState):
-    left = ((winHeight / len(currNoteState)) * note) + 5
-    top = (winWidth / 4) * 3
-    width = 50
-    height = 20
-    keyRects.append([note, [left, top, width, height]])
+    keyRects.append(makeNoteRect(note, 20))
 currNoteMapping = {K_a : 0, K_s : 1, K_d : 2}
 allNotes = []
 noteRects = [] #note, (left, top, width, height)
 
-def updatePyRects():
+
+def updateNoteRects():
     global noteRects
     for rect in noteRects:
         note = rect[0]
         rect[1][1] -= 1 #move top up
-        if not currNoteState[note]:
-            rect[1][3] += 1 #increase height
+    for note, val in enumerate(currNoteState):
+        if val:
+            lastRect = next(x for x in reversed(noteRects) if x[0] == note)
+            lastRect[1][3] += 1 #increase height
 
 while True:
     windowSurfaceObj.fill(blackColor)
-    updatePyRects()
+    updateNoteRects()
     for rect in keyRects:
         pygame.draw.rect(windowSurfaceObj, getNoteColor(rect[0]), rect[1])
     for rect in noteRects:
@@ -58,8 +62,7 @@ while True:
                 note = [noteNum, pygame.time.get_ticks(), -1]
                 allNotes.append(note)
                 print note
-                keyRect = keyRects[noteNum][:] #a full copy
-                noteRects.append(keyRect)
+                noteRects.append(makeNoteRect(noteNum, 1))
             if event.key == K_ESCAPE:
                 pygame.event.post(pygame.event.Event(QUIT))
         elif event.type == KEYUP:
