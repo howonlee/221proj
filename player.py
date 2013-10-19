fe492d3b439712c6d4847cdcef08f46494b9489a
Howon Lee <howon@thebackplane.com>
2013-10-18 20:51:09 -0700

    colors work properly

--- player.py
+++ player.py
@@ -4,26 +4,40 @@ from pygame.locals import *
 pygame.init()
 fpsClock = pygame.time.Clock()
 
-windowSurfaceObj = pygame.display.set_mode((640, 640))
+winHeight = 640
+winWidth = 640
+windowSurfaceObj = pygame.display.set_mode((winHeight, winWidth))
 pygame.display.set_caption('Music Player')
 
 blackColor = pygame.Color(0, 0, 0)
 whiteColor = pygame.Color(255, 255, 255)
 def getNoteColor(note):
-    notehash = hash(note)
+    notehash = ((note + 5) * 2654435761) % (2 ** 32)
     r = (notehash & 0xFF0000) >> 16
     g = (notehash & 0x00FF00) >> 8
     b = (notehash & 0x0000FF)
+    print "r: ", r
+    print "g: ", g
+    print "b: ", b
     return pygame.Color(r, g, b)
 
 mousex, mousey = 0, 0
 currNoteState = [0, 0, 0]
+keyRects = []
+for note, val in enumerate(currNoteState):
+    top = ((winHeight / len(currNoteState)) * note) + 5
+    left = (winWidth / 4) * 3
+    width = 50
+    height = 20
+    keyRects.append([note, [top, left, width, height]])
 currNoteMapping = {K_a : 0, K_s : 1, K_d : 2}
 allNotes = []
 noteRects = [] #note, (top, left, width, height)
 
 while True:
     windowSurfaceObj.fill(blackColor)
+    for rect in keyRects:
+        pygame.draw.rect(windowSurfaceObj, getNoteColor(rect[0]), rect[1])
     for rect in noteRects:
         pygame.draw.rect(windowSurfaceObj, getNoteColor(rect[0]), rect[1])
     for event in pygame.event.get():
