cec66ea815aca0924f2acf994e8417bfdb8ce77f
Howon Lee <howon@thebackplane.com>
2013-11-27 23:35:26 -0800

    finished, without debugging, of hmm

--- hmm.py
+++ hmm.py
@@ -17,8 +17,9 @@ class HMM:
         Learns from list of observation seq, and associated ground truths
         obs: list of list of ints in {0, ... nObs-1}, which is list of observed sequences.
         ground_truth: list of list of ints in {0, ... nStates - 1} which is associated list of ground truths.
+        Neither of these are np arrays!
         """
-        assert(len(obs) == len(ground_truths))
+        assert(len(obs) == len(ground_truth))
         self.__init__(self.nStates, self.nObs) #reset
         for i, ob in enumerate(obs):
             ground = ground_truth[i]
@@ -31,18 +32,14 @@ class HMM:
             self.emis[ ground[j], ob[j] ] += 1
 
         #normalize and convert to log
-        self.pi = np.divide(self.pi, len(obs))
         nplog = np.vectorize(self._convert_to_log)
-        self.pi = nplog(self.pi)
-        for i in range(self.nStates):
-            Zt = numpy.sum(self.trans, 1)[i] #is this the right axis?
-            Ze = numpy.sum(self.emis, 1)[i] #is this the right axis?
-            for j in range(self.nStates):
-                self.trans[i, j] /= max(1, Zt)
-            for j in range(self.nObs):
-                self.emis[i, j] /= max(1, Ze)
-            self.trans[i, :] = nplog(self.trans[i, :])
-            self.emis[i, :] = nplog(self.emis[i, :])
+        self.pi = nplog(np.divide(self.pi, len(obs)))
+        Zt = self.trans.sum(axis=1)
+        Zt[Zt < 1] = 1
+        Ze = self.emis.sum(axis=1)
+        Ze[Ze < 1] = 1
+        self.trans = nplog(self.trans / Zt[:, np.newaxis])
+        self.emis = nplog(self.emis / Ze[:, np.newaxis])
 
     def _convert_to_log(val):
         if val > 0:
@@ -54,6 +51,7 @@ class HMM:
         """
         Viterbi inference of highest likelihood hidden states seq given observations
         O(|obs| * nStates^2)
+        Observations is not numpy arrays!
         """
         tab = np.zeros((len(obs), self.nStates))
         backtrack = np.zeros((len(obs), self.nStates))
@@ -73,12 +71,15 @@ class HMM:
                 assert(smax > -1 and smax < self.nStates)
                 tab[i, j] = self.emis[j, obs[i]] + maxval
                 backtrack[i, j] = smax
-        smax = -1
-        llike = float('-inf')
-        for s in range(self.nStates):
-            if llike < tab[len(obs) -1, s]:
-                llike = tab[len(obs) - 1, s]
-                smax = s
+        llike = np.amax(tab, axis=1)
+        smax = np.argmax(tab, axis=1) #hopefully one argmax
+        assert(type(smax) == "int")
+        #smax = -1
+        #llike = float('-inf')
+        #for s in range(self.nStates):
+            #if llike < tab[len(obs) -1, s]:
+                #llike = tab[len(obs) - 1, s]
+                #smax = s
 
         best = np.zeros(len(obs))
         best.fill(-1)
