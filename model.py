55040f5aaf2e55727e3ca3761972dc4a30db04a2
Howon Lee <howon@thebackplane.com>
2013-11-29 13:59:20 -0800

    range troubles

--- model.py
+++ model.py
@@ -12,6 +12,9 @@ nottingham = cPickle.load(file("./data/Nottingham.pickle"))
 print "finished loading nottingham..."
 """
 jsb = cPickle.load(file("./data/JSB Chorales.pickle"))
+maxNote = 96
+minNote = 43
+noteRange = 96 - 43 + 1
 print "finished loading jsb..."
 
 #this is a multinomial NB
@@ -20,20 +23,20 @@ def trainNB(data):
     N = len(data)
     prior = collections.defaultdict(int)
     condprob = {}
-    for i in range(67, 97):
+    for i in range(minNote, maxNote):
         condprob[i] = collections.defaultdict(lambda: 0.002)
     for ls in data:
         for quad in ls:
             if (quad): #needed because some quads are null
                 for q in quad:
-                    if q < 67 or q > 97: continue
+                    if q < minNote or q > maxNote: continue
                     prior[q] += 1
             if (len(quad) - 1 > 0):
                 classVal = quad[-1]
                 for q in quad[:-1]: #in other words, all the other q's
-                    if q < 67 or q > 97: continue
+                    if q < minNote or q > maxNote: continue
                     condprob[classVal][q] += 1
-    for i in range(67, 97):
+    for i in range(minNote, maxNote):
         for j in condprob[i]:
             #there exist smarter ways of normalizing...
             condprob[i][j] = math.log(condprob[i][j] / prior[i]) #currently, priors are just counts
@@ -42,28 +45,28 @@ def trainNB(data):
 
 def trainMM(data):
     N = len(data)
-    model = np.zeros((30, 30))
+    model = np.zeros((noteRange, noteRange))
     for ls in data:
         for quad in ls:
             if (quad): #needed because some quads are null
                 for idx, q in enumerate(quad):
                     if idx > 0:
-                        currNote = q - 67
-                        prevNote = quad[idx - 1] - 67
+                        currNote = q - minNote
+                        prevNote = quad[idx - 1] - minNote
                         model[currNote, prevNote] += 1
     return (model, None)
 
 def trainMMOrder3(data):
     N = len(data)
-    model = np.zeros((30, 30, 30))
+    model = np.zeros((noteRange, noteRange, noteRange))
     for ls in data:
         for quad in ls:
             if (quad): #needed because some quads are null
                 for idx, q in enumerate(quad):
                     if idx > 2:
-                        currNote = q - 67
-                        prevNote = quad[idx - 1] - 67
-                        prevNote2 = quad[idx - 2] - 67
+                        currNote = q - minNote
+                        prevNote = quad[idx - 1] - minNote
+                        prevNote2 = quad[idx - 2] - minNote
                         model[currNote, prevNote, prevNote2] += 1
     return (model, None)
 
@@ -71,21 +74,24 @@ def trainHMM(data):
     """Todo: link into HMM class"""
     """ next action: figure out how the hidden states in HMM will work """
     """ or maybe say that the hidden state is the first member of the quad? """
-    nStates = 30
-    nObs = 30
-    model = HMM(nStates, nObs)
+    model = HMM(noteRange, noteRange)
     obs = []
     ground = []
     for ls in data:
         for quad in ls:
             if (quad): #needed because some quads are null
-                obs.append(map(lambda x: x - 67, quad))
-                ground.append([quad[0] - 67] * len(quad))
+                obs.append(map(lambda x: x - minNote, quad))
+                ground.append([quad[0] - minNote] * len(quad))
+    print "=================== obs ==============="
+    print obs
+    print "=================== ground ==============="
+    print ground
+
     model.learn(obs, ground) #this is a bit wrong
 
 def trainQLearning(data):
     actions = []
-    for i in range(67, 97):
+    for i in range(minNote, maxNote):
         actions.append(i)
     q = QLearner(actions, epsilon=0.1, alpha=0.2, gamma=0.9)
     for ls in data:
@@ -101,7 +107,7 @@ def trainQLearning(data):
 
 def makeNBPred(datapoint, prior, condprob):
     classes = collections.defaultdict(int)
-    for i in range(67, 97):
+    for i in range(minNote, maxNote):
         classes[i] = prior[i]
         for t in datapoint:
             classes[i] += condprob[i][t]
@@ -112,15 +118,15 @@ def makeNBPred(datapoint, prior, condprob):
 
 def makeMMPred(datapoint, model, _):
     """Todo: make probabilistic"""
-    last = datapoint[-1] - 67
-    val = np.argmax(model[:, last]) + 67 #use np.random.choice(67, 97, p=something)
+    last = datapoint[-1] - minNote
+    val = np.argmax(model[:, last]) + minNote #use np.random.choice(67, 97, p=something)
     return val
 
 def makeMM3Pred(datapoint, model, _):
     """Todo: make probabilistic"""
-    prev1 = datapoint[-1] - 67
-    prev2 = datapoint[-2] - 67
-    val = np.argmax(model[:, prev1, prev2]) + 67 #use np.random.choice(67, 97, p=something)
+    prev1 = datapoint[-1] - minNote
+    prev2 = datapoint[-2] - minNote
+    val = np.argmax(model[:, prev1, prev2]) + maxNote #use np.random.choice(67, 97, p=something)
     return val
 
 def makeHMMPred(datapoint, model, _):
