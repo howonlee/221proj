727b4ac1f78e65616e000bce0509bb741877ac64
Howon Lee <howon@howonlee.com>
2013-12-13 02:21:39 -0800

    ks warnings

--- kstester2.py
+++ kstester2.py
@@ -7,10 +7,9 @@ assert len(sys.argv) == 3
 data = map(itemgetter(0), cPickle.load(file(sys.argv[1])))
 data2 = map(itemgetter(0), cPickle.load(file(sys.argv[2])))
 
-print data2
-#why is this so much easier goddamn it
-
-#1. ks_2samp on the pred vs uniform pred
+#don't forget to munge the data properly:
+#ks test is for equality of 1-d probability distributions, to compare sample with reference distribution, or to compared two samples
+#this HAS to be two samples, alright
 k1, p1 = stats.ks_2samp(data, data2)
 print "k betw samples is: ", k1
 print "p betw samples is: ", p1
