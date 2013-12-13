78e50c94cef309ec6ebd9967860540bccb4009d3
Howon Lee <howon@howonlee.com>
2013-12-12 21:48:55 -0800

    put the old stuff here, are we sure about this

--- kstester.py
+++ kstester.py
@@ -1,1 +1,21 @@
-import scipy
+import scipy, pickle, sys
+from scipy import stats
+
+assert len(sys.argv) == 2
+data = sys.argv[1]
+
+uniform = []
+#compare against uniform: draw here
+
+jsb = []
+#compare against js bach: draw here
+
+#1. ks_2samp on the pred vs uniform pred
+k1, p1 = stats.ks_2samp(data, uniform)
+print "k against uniform samples is: ", k1
+print "p against uniform samples is: ", p1
+#2. ks_2samp on the pred vs bach pred
+k2, p2 = stats.ks_2samp(data, jsb)
+print "k against jsb samples is: ", k2
+print "p against jsb samples is: ", p2
+
