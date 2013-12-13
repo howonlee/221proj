ce67c40bb278e1908fa32da25ba7869e5460fd57
Howon Lee <howon@howonlee.com>
2013-12-12 23:36:56 -0800

    kstester update

--- kstester.py
+++ kstester.py
@@ -1,14 +1,46 @@
-import scipy, pickle, sys
+import scipy, cPickle, sys, random
 from scipy import stats
+import numpy as np
 
 assert len(sys.argv) == 2
 data = sys.argv[1]
 
-uniform = []
-#compare against uniform: draw here
+uniform = [random.randint(1, 12) for i in len(data)]
+#goddamn it
+jsbdata = cPickle.load(file("./data/JSB Chorales.pickle")) #put in actual jsb file
+jsbdist = jsbdata['train']
+jsbdist = [filter(lambda x: len(x) == 4, l) for l in jsbdist]
+jsbdist = [filter(lambda x: not(np.allclose(list(x), [0.,0.,0.,0.])), l) for l in jsbdist]
+jsbmodel = np.zeros((12,12,12))
+for ls in jsbdist:
+  for quadidx, quad in enumerate(ls):
+    tempquad = map(lambda x: x % 12, quad)
+    for idx, note in enumerate(quad):
+      if idx > 2:
+        currNote = note - minNote
+        prevNote = quad[idx - 1] - minNote
+        prevNote2 = quad[idx - 2] - minNote
+        jsbmodel[currNote, prevNote, prevNote2] += 1
+def normalizeVec(vec):
+    vecsum = vec.sum()
+    return vec / vecsum
+def jsbdraw(prev=None, prev2=None):
+  if prev is None:
+    probs = normalizeVec(jsbmodel.sum(axis=2).sum(axis=1)[:] + 0.05)
+  elif prev2 is None:
+    probs = normalizeVec(jsbmodel.sum(axis=2)[:, prev] + 0.05)
+  else:
+    probs = normalizeVec(jsbmodel[:, prev, prev2] + 0.05)
+  return np.random.choice(np.arange(1, 13), p=probs)
 
 jsb = []
-#compare against js bach: draw here
+for i in len(data):
+  if len(jsb) >= 2:
+    jsb.append(jsbdraw(prev=jsb[-1], prev2=jsb[-2]))
+  elif len(jsb) >= 1:
+    jsb.append(jsbdraw(prev=jsb[-1]))
+  else:
+    jsb.append(jsbdraw())
 
 #1. ks_2samp on the pred vs uniform pred
 k1, p1 = stats.ks_2samp(data, uniform)
